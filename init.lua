0c59215a8bb404ab13e0090be998b2175bd3a6d0
Conrad Gray <hello@conradthegray.com>
2025-06-30 08:24:54 +0100

    feat: replace telescope with snacks

--- init.lua
+++ init.lua
@@ -176,46 +176,6 @@ require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
 
-  -- NOTE: Plugins can also be added by using a table,
-  -- with the first argument being the link and the following
-  -- keys can be used to configure plugin behavior/loading/etc.
-  --
-  -- Use `opts = {}` to automatically pass options to a plugin's `setup()` function, forcing the plugin to be loaded.
-  --
-
-  -- Alternatively, use `config = function() ... end` for full control over the configuration.
-  -- If you prefer to call `setup` explicitly, use:
-  --    {
-  --        'lewis6991/gitsigns.nvim',
-  --        config = function()
-  --            require('gitsigns').setup({
-  --                -- Your gitsigns configuration here
-  --            })
-  --        end,
-  --    }
-  --
-
-  -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
-  --
-  -- This is often very useful to both group configuration, as well as handle
-  -- lazy loading plugins that don't need to be loaded immediately at startup.
-  --
-  -- For example, in the following configuration, we use:
-  --  event = 'VimEnter'
-  --
-  -- which loads which-key before all the UI elements are loaded. Events can be
-  -- normal autocommands events (`:help autocmd-events`).
-  --
-  -- Then, because we use the `opts` key (recommended), the configuration runs
-  -- after the plugin has been loaded as `require(MODULE).setup(opts)`.
-
-  -- NOTE: Plugins can specify dependencies.
-  --
-  -- The dependencies are proper plugin specifications as well - anything
-  -- you do for a plugin at the top level, you can do for a dependency.
-  --
-  -- Use the `dependencies` key to specify the dependencies of a particular plugin
-
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
---
+++ lua/custom/plugins/snacks.lua
@@ -0,0 +1,163 @@
+return {
+  {
+    'folke/snacks.nvim',
+    priority = 1000,
+    lazy = false,
+    picker = {
+      enabled = true,
+      matchers = {
+        frecency = true,
+        cwd_bonus = false,
+      },
+      formatters = {
+        file = {
+          filename_first = false,
+          filename_only = false,
+          icon_width = 2,
+        },
+      },
+      layout = {
+        -- presets options : "default" , "ivy" , "ivy-split" , "telescope" , "vscode", "select" , "sidebar"
+        -- override picker layout in keymaps function as a param below
+        preset = 'telescope', -- defaults to this layout unless overidden
+        cycle = false,
+      },
+      layouts = {
+        select = {
+          preview = false,
+          layout = {
+            backdrop = false,
+            width = 0.6,
+            min_width = 80,
+            height = 0.4,
+            min_height = 10,
+            box = 'vertical',
+            border = 'rounded',
+            title = '{title}',
+            title_pos = 'center',
+            { win = 'input', height = 1, border = 'bottom' },
+            { win = 'list', border = 'none' },
+            { win = 'preview', title = '{preview}', width = 0.6, height = 0.4, border = 'top' },
+          },
+        },
+        telescope = {
+          reverse = true, -- set to false for search bar to be on top
+          layout = {
+            box = 'horizontal',
+            backdrop = false,
+            width = 0.8,
+            height = 0.9,
+            border = 'none',
+            {
+              box = 'vertical',
+              { win = 'list', title = ' Results ', title_pos = 'center', border = 'rounded' },
+              { win = 'input', height = 1, border = 'rounded', title = '{title} {live} {flags}', title_pos = 'center' },
+            },
+            {
+              win = 'preview',
+              title = '{preview:Preview}',
+              width = 0.50,
+              border = 'rounded',
+              title_pos = 'center',
+            },
+          },
+        },
+        ivy = {
+          layout = {
+            box = 'vertical',
+            backdrop = false,
+            width = 0,
+            height = 0.4,
+            position = 'bottom',
+            border = 'top',
+            title = ' {title} {live} {flags}',
+            title_pos = 'left',
+            { win = 'input', height = 1, border = 'bottom' },
+            {
+              box = 'horizontal',
+              { win = 'list', border = 'none' },
+              { win = 'preview', title = '{preview}', width = 0.5, border = 'left' },
+            },
+          },
+        },
+      },
+    },
+    keys = {
+      -- Lazygit
+      {
+        '<leader>lg',
+        function()
+          require('snacks').lazygit()
+        end,
+        desc = '[L]azy[G]it',
+      },
+      {
+        '<leader>ll',
+        function()
+          require('snacks').lazygit.log()
+        end,
+        desc = '[L]azygit [L]ogs',
+      },
+
+      -- Snacks Picker
+      {
+        '<leader>sf',
+        function()
+          require('snacks').picker.files()
+        end,
+        desc = '[S]earch [F]iles',
+      },
+      {
+        '<leader>sc',
+        function()
+          require('snacks').picker.files { cwd = vim.fn.stdpath 'config' }
+        end,
+        desc = '[S]earch [C]onfig',
+      },
+      {
+        '<leader>sw',
+        function()
+          if vim.fn.executable 'rg' == 1 then
+            require('snacks').picker.grep()
+          else
+            vim.notify('Please install ripgrep (rg) to use this feature', vim.log.levels.WARN)
+          end
+        end,
+        desc = '[S]earch [W]ord',
+      },
+      -- {
+      --   '<leader>pws',
+      --   function()
+      --     require('snacks').picker.grep_word()
+      --   end,
+      --   desc = 'Search Visual selection or Word',
+      --   mode = { 'n', 'x' },
+      -- },
+      {
+        '<leader>sk',
+        function()
+          require('snacks').picker.keymaps { layout = 'ivy' }
+        end,
+        desc = '[S]earch [K]eymaps',
+      },
+
+      -- Git Stuff
+      {
+        '<leader>gbr',
+        function()
+          require('snacks').picker.git_branches { layout = 'select' }
+        end,
+        desc = 'Pick and Switch Git Branches',
+      },
+
+      -- Other
+      {
+        '<leader>sh',
+        function()
+          require('snacks').picker.help()
+        end,
+        desc = '[S]earch [H]elp Pages',
+      },
+    },
+  },
+}
