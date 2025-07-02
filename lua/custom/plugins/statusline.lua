c085ccd561ccacc514b1f76a6b38623a1c9b33dd
Conrad Gray <hello@conradthegray.com>
2025-07-02 18:55:53 +0100

    feat: added notifications and status line config

---
+++ lua/custom/plugins/statusline.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    'nvim-lualine/lualine.nvim',
+    config = function()
+      require('lualine').setup {
+        options = {
+          theme = 'catppuccin', -- Use the Catppuccin theme for lualine
+          section_separators = '', -- Optional: adjust separators as needed
+          component_separators = '',
+          icons_enabled = true,
+        },
+        dependencies = { 'nvim-tree/nvim-web-devicons' },
+        -- You can further customise sections here:
+        sections = {
+          lualine_a = { 'mode' },
+          lualine_b = { 'branch', 'diff', 'diagnostics' },
+          lualine_c = { 'filename' },
+          lualine_x = { 'filetype' },
+          lualine_y = { 'progress' },
+          lualine_z = { 'location' },
+        },
+      }
+    end,
+  },
+}
