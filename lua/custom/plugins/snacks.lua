59411c2c754e7ff7fd74ff3d8444e330978d6cae
Conrad Gray <hello@conradthegray.com>
2025-07-02 20:07:47 +0100

    feat: update dashboard

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -6,6 +6,64 @@ return {
     ---@type snacks.Config
     opts = {
       dashboard = {
+        ---@class snacks.dashboard.Config
+        preset = {
+          keys = {
+            {
+              icon = ' ',
+              key = 'f',
+              desc = 'Find File',
+              action = function()
+                require('snacks').picker.files()
+              end,
+            },
+            {
+              icon = ' ',
+              key = 'n',
+              desc = 'New File',
+              action = ':ene | startinsert',
+            },
+            {
+              icon = ' ',
+              key = 'g',
+              desc = 'Find Text',
+              action = ":lua Snacks.dashboard.pick('live_grep')",
+            },
+            {
+              icon = ' ',
+              key = 'r',
+              desc = 'Recent Files',
+              action = ":lua Snacks.dashboard.pick('oldfiles')",
+            },
+            {
+              icon = ' ',
+              key = 'c',
+              desc = 'Config',
+              action = function()
+                require('snacks').picker.files { cwd = vim.fn.stdpath 'config' }
+              end,
+            },
+            {
+              icon = ' ',
+              key = 's',
+              desc = 'Restore Session',
+              section = 'session',
+            },
+            {
+              icon = '󰒲 ',
+              key = 'L',
+              desc = 'Lazy',
+              action = ':Lazy',
+              enabled = package.loaded.lazy ~= nil,
+            },
+            {
+              icon = ' ',
+              key = 'q',
+              desc = 'Quit',
+              action = ':qa',
+            },
+          },
+        },
         sections = {
           { section = 'header' },
           { icon = ' ', title = 'Keymaps', section = 'keys', indent = 2, padding = 1 },
