1e2af8a42a1fd94cda141db988dd612e860ed514
Conrad Gray <hello@conradthegray.com>
2025-07-13 08:37:54 +0100

    refactor: move files around

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
-require 'vim-settings'
-require 'keymaps'
-require 'auto-commands'
-require 'setup-lazy'
+require 'config.vim-settings'
+require 'config.keymaps'
+require 'config.auto-commands'
+require 'config.setup-lazy'
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
